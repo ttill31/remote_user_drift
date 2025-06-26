cd30932a4df25984b87365b1cc4a47234df9e726
ttill31 <87537001+ttill31@users.noreply.github.com>
2025-06-26 08:20:17 -0600

    Added state field

--- entra_user.py
+++ entra_user.py
@@ -5,3 +5,7 @@ class EntraUser:
     id: str
     display_name: str
     ip_addresses: set[str]
+    states: set[str]
+
+    def __hash__(self) -> int:
+        return len(self.id) % len(self.display_name)
